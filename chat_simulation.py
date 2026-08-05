54cd58f2bcc4b317775d2663eb7dad2268841f2b
Mayko Rodrigues <mak213k@yahoo.com.br>
2026-08-04 21:50:15 -0300

    Create chat_simulation.py
    orientações como instalar

---
+++ chat_simulation.py
@@ -0,0 +1,6 @@
+# lembrem destes comandos quando instalar o chatterbot
+# pip install chatterbot
+# python -m pip install --upgrade pip  
+# pip install chatterbot_corpus          
+# pip install clic
+# python -m spacy download en_core_web_sm
